2e7668915819d569b9e7507e989f2f9d43a5bb9f
Team Patient <reut@hack.com>
2017-05-04 14:10:33 +0300

    Initial commit.

---
+++ app/src/main/java/hack/reuth/pokedoc/MyPrescriptionsActivitiy.java
@@ -0,0 +1,13 @@
+package hack.reuth.pokedoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPrescriptionsActivitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_prescriptions_activitiy);
+    }
+}
---
+++ app/src/main/java/hack/reuth/pokedoc/ReportPainActivity.java
@@ -0,0 +1,13 @@
+package hack.reuth.pokedoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportPainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_pain);
+    }
+}
